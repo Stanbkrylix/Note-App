d0195496d0697ef4feac5622b5e62cfedbdb4015
Stanley Delva <juniordelva1@gmail.com>
2024-07-18 09:36:49 -0400

    display section responsible mobile css has added

--- mobile.js
+++ mobile.js
@@ -4,8 +4,12 @@ export default function mobileFunctionalities() {
     const projectSection = document.querySelector(".project-section");
     const notesSection = document.querySelector(".notes-section");
     const displayNoteSection = document.querySelector(".display-note-section");
+    const displaySectionWrapper = document.querySelector(
+        ".display-section-wrapper"
+    );
 
     const overlayModal = document.querySelector(".overlay-modal");
+    const overlayModal2 = document.querySelector(".overlay-modal2");
     const menu = document.querySelector(".menu-btn");
     const rightArrow = document.querySelector(".right-arrow");
     const displayLeftArrow = document.querySelector(".display-left-btn");
@@ -21,6 +25,22 @@ export default function mobileFunctionalities() {
         if (projectSection.classList.contains("move-left")) {
             projectSection.classList.remove("move-left");
             overlayModal.classList.remove("move-left");
+        }
+    });
+
+    displayLeftArrow.addEventListener("click", (e) => {
+        console.log(e.target);
+        if (!displaySectionWrapper.classList.contains("move-right")) {
+            displaySectionWrapper.classList.add("move-right");
+            overlayModal2.classList.add("move-right");
+        }
+    });
+
+    rightArrow.addEventListener("click", (e) => {
+        console.log(e.target);
+        if (displaySectionWrapper.classList.contains("move-right")) {
+            displaySectionWrapper.classList.remove("move-right");
+            overlayModal2.classList.remove("move-right");
         }
     });
 }
